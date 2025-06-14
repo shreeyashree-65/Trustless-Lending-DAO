03751dcd669b6fed3399ebc4d6eaeb672c3a78a2
shreeyashree-65 <1ds22cb045@dsce.edu.in>
2025-06-14 19:29:48 +0530

    -

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import Card from './components/ui/card';
+import Button from './components/ui/button';
 import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import { CONTRACT_ABI, CONTRACT_ADDRESS } from './constants';
